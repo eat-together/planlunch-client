cf16e9c936c668e723544751957604eb5860777e
pogopaule <pogopaule@gmx.de>
2014-08-17 16:13:46 +0200

    refactor hover-row to component and rename to hover-place

---
+++ app/components/hover-place.js
@@ -0,0 +1,20 @@
+export default Ember.Component.extend({
+  tagName: 'li',
+  isActive: false,
+  classNameBindings: ['isActive:highlighted'],
+
+  mouseEnter: function() {
+    this.set('hoveredPlace', this.get('place'));
+  },
+  mouseLeave: function() {
+    this.set('hoveredPlace', null);
+  },
+
+  hoveredPlaceChanged: function() {
+    if(this.get('hoveredPlace.name') === this.get('place.name')) {
+      this.set('isActive', true);
+    } else {
+      this.set('isActive', false);
+    }
+  }.observes('hoveredPlace')
+});
