3d2ee030ad1f2226d4773ddfe573d5898b240204
pogopaule <pogopaule@gmx.de>
2014-08-18 21:27:55 +0200

    add better validation message

--- app/controllers/askname.js
+++ app/controllers/askname.js
@@ -1,8 +1,13 @@
 export default Ember.Controller.extend({
   actions: {
     save: function() {
-      localStorage.setItem('user.name', this.get('name'));
-      this.transitionToRoute('index');
+      var name = this.get('name');
+      if(!name.match(/^[a-z]{3,4}$/)) {
+        this.set('error', 'Das Kürzel hat 3 oder 4 Kleinbuchstaben!');
+      } else {
+        localStorage.setItem('user.name', this.get('name'));
+        this.transitionToRoute('index');
+      }
     }
   }
 });
