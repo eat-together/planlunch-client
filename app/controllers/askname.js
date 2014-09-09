e41c2d55eea0126c78fae82543523fa15c36fab4
pogopaule <pogopaule@gmx.de>
2014-09-09 19:50:13 +0200

    inline variable

--- app/controllers/askname.js
+++ app/controllers/askname.js
@@ -1,8 +1,7 @@
 export default Ember.Controller.extend({
   actions: {
     save: function() {
-      var name = this.get('name');
-      if(!name.match(/^[a-z]{3,4}$/)) {
+      if(!this.get('name').match(/^[a-z]{3,4}$/)) {
         this.set('error', 'Das Kürzel hat 3 oder 4 Kleinbuchstaben!');
       } else {
         localStorage.setItem('user.name', this.get('name'));
