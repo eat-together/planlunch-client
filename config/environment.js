6c264903b7ee7ec02ab1cee104c1c46587fa876b
pogopaule <pogopaule@gmx.de>
2014-12-24 13:31:04 +0100

    move askname to pod

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'planlunch',
+    podModulePrefix: 'planlunch/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
