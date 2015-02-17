cc58c5c964c5701fb2f4bb3b67cc2013bf26364d
pogopaule <pogopaule@gmx.de>
2015-02-17 21:10:37 +0100

    don't use proxy anymore

--- config/environment.js
+++ config/environment.js
@@ -27,8 +27,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-    ENV.API_URL = '';
-
+    ENV.API_URL = 'http://localhost:3000/';
   }
 
   if (environment === 'test') {
